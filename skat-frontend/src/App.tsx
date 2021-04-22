a8434c317cd61693d4f324abc2d6674544670bdd
pinguly <nicolasfaross@gmail.com>
2021-04-22 09:58:55 +0200

    fancy cards positioning

--- skat-frontend/src/App.tsx
+++ skat-frontend/src/App.tsx
@@ -25,18 +25,69 @@ const Card: React.FC<{ card: ICard, onClick?: () => void, player?: string }> = (
 }
 
 const Hand: React.FC<{ cards: ICard[] }> = ({ cards }) => {
-  return <div style={{ display: 'flex', fontSize: '.8em' }}>
-    {cards.map((card, index) => {
-      const degreeSpread = 20
-      const magicFactor = 15   // FIXME >:(
-      let f = (index + .5) / cards.length
-      let rot = degreeSpread * (f - .5)
-      let rrot = rot * Math.PI / 180
+  return <div style={{ display: 'flex', fontSize: '.8em', width: 'var(--cards-width)' }}>
+    {
+    cards.map((card, index) => {
+      let ratio = 84.9667 / 122.567   // ratio cardWidth/cardHeight (including colored border)
+      let width = 6;                  // with of the arc (unit: card widths)
+      let theta = 27 * Math.PI / 180; // inner angle of the arc (unit: rad)
+
+      let a = (cards.length - 1 - width) / 2; // leftmost x position of arc
+      let b = (cards.length - 1 + width) / 2; // rightmost x position of arc
+
+      // magic math follows
+      let h = 1e-8;
+      let n = 500;
+
+      function f(t: number) {
+          let w1 = Math.sin(theta)
+          let w2 = Math.cos(theta)
+          let z1 = Math.cos(t * Math.PI)
+          let z2 = Math.sin(t * Math.PI)
+          let x = ((a + b) * (w2 * z2 + 1) + (a - b) * z1) / (2 * w2 * z2 + 2)
+          let y = (b - a) * w1 * z2 / (2 * w2 * z2 + 2)
+          return [x, y]
+      }
+
+      function df(t: number) {
+          let [x, y] = f(t)
+          let [x2, y2] = f(t + h);
+          let [dx, dy] = [(x2 - x) / h, (y2 - y) / h];
+          return [dx, dy];
+      }
+
+      function arclengthParam(t: number) {
+          let res = 0;
+          let cache = []; // TODO compute once outside loop
+          cache.length = n;
+          for (let i = 0; i < n; i++) {
+              let [ax, ay] = df(i / n);
+              let a = Math.sqrt(ax*ax + ay*ay);
+              let [bx, by] = df((i + 1) / n);
+              let b = Math.sqrt(bx*bx + by*by);
+              res += 0.5 * (a + b) / n;
+              cache[i] = res;
+          }
+          for (let i = 0; i < n; i++) {
+              if (cache[i] > t * res) {
+                  return (i) / n;
+              }
+          }
+          return 1;
+      }
+
+      let t = index / (cards.length - 1); // position on arc between 0 and 1
+      let u = arclengthParam(t);
+      let [x, y] = f(u);
+      let [dx, dy] = df(u);
+      let r = -Math.atan2(dy, dx);
+
       return <span style={{
-        transform: `translate(0em, ${Math.abs(Math.sin(rrot)) * magicFactor}em) rotate(${rot}deg)`,
-        transformOrigin: '50% 50%',
-      }}><Card card={card} onClick={() => alert("aluurm " + JSON.stringify(card))}></Card></span>
-    })}
+        transform: `translate(${(x-index)*100}%, ${-ratio*y*100}%) rotate(${r}rad)`,
+        transformOrigin: 'center center',
+        width: ""
+    }}><Card card={card} onClick={() => alert("aluurm " +  JSON.stringify(card))}></Card></span>
+  })}
   </div>
 }
 
