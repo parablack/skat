386b912f9fdd1d828ef97acb8c998bde2298f0f0
pinguly <nicolasfaross@gmail.com>
2021-04-23 20:19:21 +0200

    'explicit' card curve

--- skat-frontend/src/Hand.tsx
+++ skat-frontend/src/Hand.tsx
@@ -7,59 +7,63 @@ const ratio = 84.9667 / 122.567   // ratio cardWidth/cardHeight (including color
 const width = 6;                  // with of the arc (unit: card widths)
 const theta = 27 * Math.PI / 180; // inner angle of the arc (unit: rad)
 
+const cosT = Math.cos(theta);
+const sinT = Math.sin(theta);
+
 // magic math follows
-const h = 1e-8;
-const n = 500;
 
 function f(t: number, a: number, b: number) {
-    let w1 = Math.sin(theta)
-    let w2 = Math.cos(theta)
-    let z1 = Math.cos(t * Math.PI)
-    let z2 = Math.sin(t * Math.PI)
-    let x = ((a + b) * (w2 * z2 + 1) + (a - b) * z1) / (2 * w2 * z2 + 2)
-    let y = (b - a) * w1 * z2 / (2 * w2 * z2 + 2)
-    return [x, y]
+
+    // reparametrization
+
+    let r = Math.sqrt(4 - 2 * cosT);
+
+    let alpha = 0;
+    alpha += Math.atan2(2 - r, - 2 * cosT) * t
+    alpha -= Math.atan2(2 - r,   2 * cosT) * (t - 1)
+    alpha += Math.atan2(2 + r,   2 * cosT) * (t - 1)
+    alpha -= Math.atan2(2 + r, - 2 * cosT) * t
+
+    let d = 1;
+    d *= 2 - cosT - 2 * r + 2 + 2 * cosT * cosT;
+    d /= 2 - cosT + 2 * r + 2 + 2 * cosT * cosT;
+
+    let zr = -2 * r * Math.sin(alpha) * Math.sqrt(d);
+    let zi = (-2 - r) * d + r + 4* Math.cos(alpha) * Math.sqrt(d) - 2;
+    let zn = Math.sqrt(zr * zr + zi * zi)
+
+    let cosH = zr / zn;
+    let sinH = zi / zn;
+
+    // actual function: H = s * pi, 0 < s < 1
+
+    let fr = 2 * cosT * sinH + 2;
+    let fx = (a + b) * (cosT * sinH + 1) + (a - b) * cosH;
+    let fy = (b - a) * sinH * sinT;
+
+    let dfr = 2 * cosT * cosH;
+    let dfx = (a + b) * (cosT * cosH) - (a - b) * sinH;
+    let dfy = (b - a) * cosH * sinT;
+
+    let xx = (dfx * fr - fx * dfr) / fr / fr;
+    let yy = (dfy * fr - fy * dfr) / fr / fr;
+
+    let rot = -Math.atan2(yy, xx);
+    return [fx / fr, fy / fr, rot];
 }
 
-function df(t: number, a: number, b: number) {
-    let [x, y] = f(t, a, b)
-    let [x2, y2] = f(t + h, a, b);
-    let [dx, dy] = [(x2 - x) / h, (y2 - y) / h];
-    return [dx, dy];
-}
-
-function arclengthParam(t: number, a_: number, b_: number) {
-    let res = 0;
-    let cache = []; // TODO compute once outside loop
-    cache.length = n;
-    for (let i = 0; i < n; i++) {
-        let [ax, ay] = df(i / n, a_, b_);
-        let a = Math.sqrt(ax * ax + ay * ay);
-        let [bx, by] = df((i + 1) / n, a_, b_);
-        let b = Math.sqrt(bx * bx + by * by);
-        res += 0.5 * (a + b) / n;
-        cache[i] = res;
-    }
-    for (let i = 0; i < n; i++) {
-        if (cache[i] > t * res) {
-            return (i) / n;
-        }
-    }
-    return 1;
-}
 
 export const Hand: React.FC<{ cards: ICard[], onClickCard: (card: ICard) => void }> = ({ cards, onClickCard }) => {
-    return <div style={{ display: 'flex', fontSize: '.8em', width: 'var(--cards-width)' }}>
+    return <div style={{ display: 'flex', fontSize: '.8em' }}>
         {
             cards.map((card, index) => {
                 let a = (cards.length - 1 - width) / 2; // leftmost x position of arc
                 let b = (cards.length - 1 + width) / 2; // rightmost x position of arc
 
                 let t = index / (cards.length - 1); // position on arc between 0 and 1
-                let u = arclengthParam(t, a, b);
-                let [x, y] = f(u, a, b);
-                let [dx, dy] = df(u, a, b);
-                let r = -Math.atan2(dy, dx);
+
+                let [x, y, r] = f(t, 0, b - a);
+                x += a
 
                 return <span key={index} style={{
                     transform: `translate(${(x - index) * 100}%, ${-ratio * y * 100}%) rotate(${r}rad)`,
