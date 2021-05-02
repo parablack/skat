455c0d18b4fce2d77a2005eff58f7fc786915ed2
Mrmaxmeier <Mrmaxmeier@gmail.com>
2021-05-02 23:58:12 +0200

    web: connect to wss if on https

--- skat-frontend/src/index.tsx
+++ skat-frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
 
-const ws = new WebSocket(`ws://${window.location.hostname}:8080`)
+const ws = new WebSocket(`${window.location.protocol === "https:" ? 'wss' : 'ws'}://${window.location.hostname}:8080`)
 ws.onopen = () => {
   console.log("ws opened")
   let nickname = localStorage.getItem("nickname")
